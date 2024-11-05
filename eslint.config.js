4a30f8f8d29658a60aa007aaed2b98fe34d50798
hieule0207Zigvy <hieulm@zigvy.com>
2024-11-05 20:56:15 +0700

    config eslint prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptEslintParser from '@typescript-eslint/parser'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -10,19 +13,24 @@ export default tseslint.config(
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
+      sourceType: 'module',
+      parser: typescriptEslintParser
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+      '@typescript-eslint': typescriptEslintPlugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error', // Enforce Prettier formatting as ESLint errors
+      'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+      '@typescript-eslint/no-unused-vars': 'warn', // Warn on unused variables
+      'react/prop-types': 'off' // Disable prop-types check (if using TypeScript)
+    }
+  }
 )
