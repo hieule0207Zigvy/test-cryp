cdb02d83bcac6feccd69064ade71a3a012d3f35e
hieule0207Zigvy <hieulm@zigvy.com>
2024-11-06 21:45:46 +0700

    add base style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   console.log(import.meta.env.VITE_API_URL)
 
-  return <div className='app'></div>
+  return (
+    <div className='app'>
+      <p>{'hehe'}</p>
+    </div>
+  )
 }
 
 export default App
